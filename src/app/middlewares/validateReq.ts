13e1628c0e1449bcc1d694082c8ab6a4f6db9107
Shakhawat <shakhawat2021hossain2021@gmail.com>
2025-07-17 12:20:09 +0600

    created zod schema for user validation

---
+++ src/app/middlewares/validateReq.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+
+export const validateReq = (zodSchema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        req.body = await zodSchema.parseAsync(req.body)
+        next()
+    } catch (error) {
+        next(error)
+    }
+
+}
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userControllers } from "./user.controller";
+import { validateReq } from "../../middlewares/validateReq";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router()
-router.post('/register', userControllers.createUser)
+router.post('/register', validateReq(createUserZodSchema) , userControllers.createUser)
 router.get('/users', userControllers.getAllUser)
 
 export const userRoutes = router
